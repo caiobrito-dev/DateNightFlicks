954d050476b7530af67f7c57cb09a30df6224e66
Caio Barcelos De Brito <caiobarcelosdebrito@gmail.com>
2024-06-18 19:45:36 -0300

    Requisição funcionando e Lista pessoal quase pronta

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,14 +1,15 @@
+let contador = 0
+
 async function carregarImagens() {
   const input = document.getElementById("input_film")
   const galeria = document.getElementById('galeria');
 
-
-  if(input.value != ""){
-
+  if (contador == 0){
+    contador++
     try {
-      
+    
       const response = await fetch('http://localhost:3000/imagens');
-
+  
       if (!response.ok) {
         throw new Error(`Erro HTTP! status: ${response.status}`);
       }
@@ -16,10 +17,10 @@ async function carregarImagens() {
       const imagens = await response.json();
       
       console.log(imagens)
-
-
+  
+  
       imagens.forEach(imagem => {
-
+  
         const filmData = document.createElement("div")
         filmData.innerHTML = `
           <img src="${imagem.url}" alt="">
@@ -33,20 +34,50 @@ async function carregarImagens() {
         filmData.classList.add("filmData")
         galeria.appendChild(filmData)
         galeria.style.display = "flex"
-
+        filmData.style.display = "flex"
+  
+  
       });
-
+  
       
     } catch (error) {
       console.error('Erro ao carregar imagens:', error);
     }
+  }
+  
 
-  }  else{
-      galeria.innerHTML = ""
-      galeria.style.display = "none"
-    } 
 }
 
-function filtro(){
+function Filtrar(){
+    var input,
+      filter,
+      galeria,
+      films,
+      title,
+      txtValue,
+      cont = 0
+
+  input = document.getElementById("input_film")
+  galeria = document.getElementById("galeria")
+
+  galeria.style.display = "flex"
+  filter = input.value.toUpperCase()
+
+  films = document.getElementsByClassName("filmData")
+
+  for (i = 0; i < films.length; i++){
+    title = films[i].getElementsByTagName("h2")[0]
+    txtValue = title.textContent || title.innerText
+    if( txtValue.toLocaleUpperCase().indexOf(filter) > -1){
+      films[i].style.display = "flex"
+      cont++
+    }
+    else{
+      films[i].style.display = "none"
+    }
+  }
+
   
+  
+
 }
